9c8c78981283164dd548bc17674bf3175570f914
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-08-29 20:39:28 +0200

    added generator options for jsunittest.js and loading ExtJS with the tests

--- generators/javascript_test/javascript_test_generator.rb
+++ generators/javascript_test/javascript_test_generator.rb
@@ -1,5 +1,5 @@
 class JavascriptTestGenerator < Rails::Generator::NamedBase  
-  default_options :app_assets => false
+  default_options :app_assets => false, :prototype => false, :extjs => false
 
   def manifest
     record do |m|
@@ -10,13 +10,17 @@ class JavascriptTestGenerator < Rails::Generator::NamedBase
   
   protected
   def banner
-    "Usage: #{$0} javascript_test libary_file [--use-app-assets]"
+    "Usage: #{$0} javascript_test libary_file [--use-app-assets] [--prototype] [--extjs]"
   end
 
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
+    opt.on("-P", "--prototype",
+         "Uses prototype.js with unittest.js instead of jsunittest.js") { |v| options[:prototype] = v }
     opt.on("-A", "--use-app-assets",
-           "Uses application's own version of prototype.js instead of the plugins") { |v| options[:app_assets] = v }
+         "Uses application's own version of prototype.js instead of the plugins (in conjunction with --prototype)") { |v| options[:app_assets] = v }
+    opt.on("-E", "--extjs",
+         "Adds script tags for loading ExtJs with the generated test (ExtJs is expected to be installed in public/ext)") { |v| options[:extjs] = v }
   end
 end
